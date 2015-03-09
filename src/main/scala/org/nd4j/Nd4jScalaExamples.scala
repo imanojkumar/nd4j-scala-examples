04cc2c39bd6ef67387a8312c241a5aad0fbb39bb
Adam Gibson <adam@skymind.io>
2015-03-09 14:14:43 -0400

    fix project

--- src/main/scala/org/nd4j/Nd4jScalaExamples.scala
+++ src/main/scala/org/nd4j/Nd4jScalaExamples.scala
@@ -4,7 +4,6 @@ import java.util.Arrays
 
 import org.nd4j.api.linalg.DSL._
 import org.nd4j.linalg.factory.Nd4j
-import org.nd4j.linalg.factory.Nd4j
 
 
 /**
@@ -37,8 +36,8 @@ object Nd4jScalaExamples {
     val arrT = arr.T
 
     /** Row (0) and Column (1) Sums */
-    println(Nd4j.sum(arr4, 0) + "Calculate the sum for each row")
-    println(Nd4j.sum(arr4, 1) + "Calculate the sum for each column")
+    println(Nd4j.sum(arr4, 0).toString + "Calculate the sum for each row")
+    println(Nd4j.sum(arr4, 1).toString + "Calculate the sum for each column")
 
     /** Checking array shape */
     println(Arrays.toString(arr2.shape) + "Checking array shape")
@@ -47,28 +46,28 @@ object Nd4jScalaExamples {
     println(arr2.toString() + "Array converted to string")
 
     /** Filling the array with the value 5 (same as numpy's fill method) */
-    println(arr2.assign(5) + "Array assigned value of 5 (equivalent to fill method in numpy)")
+    println(arr2.assign(5).toString + "Array assigned value of 5 (equivalent to fill method in numpy)")
 
     /** Reshaping the array */
-    println(arr2.reshape(2, 2) + "Reshaping array")
+    println(arr2.reshape(2, 2).toString + "Reshaping array")
 
     /** Raveling the array (returns a flattened array) */
-    println(arr2.ravel + "Raveling array")
+    println(arr2.ravel.toString + "Raveling array")
 
     /** Flattening the array (same as numpy's flatten method) */
-    println(Nd4j.toFlattened(arr2) + "Flattening array (equivalent to flatten in numpy)")
+    println(Nd4j.toFlattened(arr2).toString + "Flattening array (equivalent to flatten in numpy)")
 
     /** Array sorting */
-    println(Nd4j.sort(arr2, 0, true) + "Sorting array")
-    println(Nd4j.sortWithIndices(arr2, 0, true) + "Sorting array and returning sorted indices")
+    println(Nd4j.sort(arr2, 0, true).toString + "Sorting array")
+    println(Nd4j.sortWithIndices(arr2, 0, true).toString + "Sorting array and returning sorted indices")
 
     /** Cumulative sum */
-    println(Nd4j.cumsum(arr2) + "Cumulative sum")
+    println(Nd4j.cumsum(arr2).toString + "Cumulative sum")
 
     /** Basic stats methods */
-    println(Nd4j.mean(arr) + "Calculate mean of array")
-    println(Nd4j.std(arr2) + "Calculate standard deviation of array")
-    println(Nd4j.`var`(arr2), "Calculate variance")
+    println(Nd4j.mean(arr).toString + "Calculate mean of array")
+    println(Nd4j.std(arr2).toString + "Calculate standard deviation of array")
+    println(Nd4j.`var`(arr2).toString, "Calculate variance")
 
     /** Find min and max values */
     println(Nd4j.max(arr3), "Find max value in array")
